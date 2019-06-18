59cefddd2ec76dedf5c4a713c71096dc5cc3d47b
jcwise98 <jcwise@udel.edu>
2019-06-17 21:37:28 -0400

    better user interface

--- hello_world.py
+++ hello_world.py
@@ -4,29 +4,43 @@
 
 
 class HelloWorld:
-    x = 1
-
     def __init__(self, var):
-        self.num = var + self.x
-        self.phrase = "Hello World"
-
-
-userNum = int(input("What number do you wish to give?"))
-hw = HelloWorld(userNum)
-
-print("Var + your given number is:", hw.num)
-for i in range(hw.num):
-    print(hw.phrase, i+1)
+        self.num = var
+        self.phrase = "Hello World!"
 
 
 def square_num(num):
+    if num < 0:
+        num = num*-1
     val = 0
     for i in range(num):
         val = val + num
     return val
 
 
-result = square_num(userNum)
-print("\nYour given number squared is:", result)
+userInput = input("What number do you wish to give?\n")
 
-input("Press enter to exit...")
+while True:
+    while True:
+        try:
+            userNum = int(userInput)
+            hw = HelloWorld(userNum)
+            print("Your given number is:", hw.num)
+            for i in range(hw.num):
+                print(hw.phrase, i+1)
+
+            result = square_num(userNum)
+            print("\nYour given number squared is:", result)
+            break
+        except ValueError:
+            userInput = input("Please enter a number\n")
+
+    userInput = input("Enter a number to continue\nPress Q to exit...\n")
+    try:
+        userNum = int(userInput)
+    except ValueError:
+        if (userInput == 'Q') or (userInput == 'q'):
+            print("Goodbye...")
+            break
+        else:
+            userInput = input("Please enter a number\n")
