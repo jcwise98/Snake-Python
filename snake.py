be151e042ec9342ac485de49856ac52ca20cc15a
jcwise98 <jcwise@udel.edu>
2020-03-23 21:25:43 -0400

    sound and scoreboard

--- snake.py
+++ snake.py
@@ -15,25 +15,32 @@ def wall_collision():
     global x_fac
     global y_fac
     global alive
-    if snake.head.x >= (constant.SCREEN_WIDTH - constant.SNAKE_SIZE):
+    if snake.head.x > (constant.SCREEN_WIDTH - constant.SNAKE_SIZE):
         alive = False
+        # pygame.mixer.Sound.play(hit_sound)
         x_fac = 0
-    if snake.head.x <= 0:
+    if snake.head.x < 0:
         alive = False
+        # pygame.mixer.Sound.play(hit_sound)
         x_fac = 0
-    if snake.head.y >= (constant.SCREEN_HEIGHT - constant.SNAKE_SIZE):
+    if snake.head.y > (constant.SCREEN_HEIGHT - constant.SNAKE_SIZE):
         alive = False
+        # pygame.mixer.Sound.play(hit_sound)
         y_fac = 0
-    if snake.head.y <= 0:
+    if snake.head.y < constant.SCOREBOARD:
         alive = False
+        # pygame.mixer.Sound.play(hit_sound)
         y_fac = 0
 
 
 def food_collision():
+    global gameScore
     if (snake.head.x == food.x) and (snake.head.y == food.y):
         add_snake()
+        pygame.mixer.Sound.play(eat_sound)
+        gameScore += 100
         food.x = gen_random()
-        food.y = gen_random()
+        food.y = gen_random() + constant.SCOREBOARD
 
 
 def self_collision():
@@ -46,30 +53,44 @@ def self_collision():
 
 
 def add_snake():
+    global gameSpeed
     curr = snake.head
     while curr.next is not None:
         curr = curr.next
     curr.next = snakelist.SNode(curr.xprev, curr.yprev)
     snake.length += 1
-    constant.GAME_SPEED += 1
+    gameSpeed += 1
 
 
 pygame.init()
 gameDisplay = pygame.display.set_mode((constant.SCREEN_WIDTH, constant.SCREEN_HEIGHT))
 
+x_vel = 15
+y_vel = 15
+
+eat_sound = pygame.mixer.Sound("sfx/eat.wav")
+# hit_sound = pygame.mixer.Sound("hit.wav")
+lose_sound = pygame.mixer.Sound("sfx/lose.wav")
+
 background_colour = (89, 89, 89)
 snake_color = (255, 125, 64)
 food_color = (255, 250, 205)
 gameDisplay.fill(background_colour)
+pygame.draw.rect(gameDisplay, pygame.color.Color('Black'),
+                 (0, 0, constant.SCREEN_WIDTH, constant.SCOREBOARD), 0)
+
+pause_text = pygame.font.SysFont(constant.GAME_TEXT, constant.TEXT_SIZE).render('Paused - Press SPACE to resume', True,
+                                                                                pygame.color.Color('White'))
+dead_text = pygame.font.SysFont(constant.GAME_TEXT, constant.TEXT_SIZE).render('You died! Press R to restart', True,
+                                                                               pygame.color.Color('White'))
 
 pygame.display.set_caption('Snake')
 while True:
     alive = True
-    constant.GAME_SPEED = 10
+    death = 0
+    gameScore = 0
+    gameSpeed = constant.GAME_SPEED
     clock = pygame.time.Clock()
-
-    x_vel = 15
-    y_vel = 15
 
     x_fac = 0
     y_fac = 0
@@ -77,7 +98,7 @@ while True:
     snake = snakelist.SLinkedList()
     snake.head = snakelist.SNode(gen_random())
 
-    food = objects.FoodObj(gen_random(), gen_random())
+    food = objects.FoodObj(gen_random(), gen_random() + constant.SCOREBOARD)
 
     right = pygame.K_RIGHT
     left = pygame.K_LEFT
@@ -86,15 +107,15 @@ while True:
 
     RUNNING, PAUSE = 0, 1
     state = RUNNING
-    pause_text = pygame.font.SysFont('Consolas', 32).render('Paused - Press SPACE to resume', True,
-                                                            pygame.color.Color('White'))
-    dead_text = pygame.font.SysFont('Consolas', 32).render('You died! Press R to restart', True,
-                                                           pygame.color.Color('White'))
 
-    score_text = pygame.font.SysFont('Consolas', 32).render('Score: 100', True,
-                                                            pygame.color.Color('White'))
     restart = True
+    scoreDec = 11
     while restart:
+        score_text = pygame.font.SysFont('Consolas', constant.TEXT_SIZE - 10).render(('Score: ' + str(gameScore)), True,
+                                                                                     pygame.color.Color('White'))
+        segment_text = pygame.font.SysFont('Consolas', constant.TEXT_SIZE - 10).render(('Segments: ' + str(snake.length)), True,
+                                                                                     pygame.color.Color('White'))
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -128,7 +149,7 @@ while True:
                     state = PAUSE
                 if event.key == pygame.K_a:
                     snake.head.x = gen_random()
-                    snake.head.y = gen_random()
+                    snake.head.y = gen_random() + constant.SCOREBOARD
                 if event.key == pygame.K_SPACE:
                     state = RUNNING
                 if alive is False:
@@ -142,14 +163,27 @@ while True:
                 self_collision()
 
                 if alive:
+                    scoreDec += 1
+                    if gameScore > 0 and (scoreDec % 20 == 0):
+                        gameScore -= 10
                     snake.head.xprev = snake.head.x
                     snake.head.yprev = snake.head.y
                     snake.head.x += (x_vel * x_fac)
                     snake.head.y += (y_vel * y_fac)
 
                 gameDisplay.fill(background_colour)
-                # gameDisplay.blit(score_text, (snake.head.x, snake.head.y-50))
-                current = snake.head
+                pygame.draw.rect(gameDisplay, pygame.color.Color('Black'),
+                                 (0, 0, constant.SCREEN_WIDTH, constant.SCOREBOARD), 0)
+                pygame.draw.rect(gameDisplay, pygame.color.Color('White'),
+                                 (0, 0, constant.SCREEN_WIDTH, constant.SCOREBOARD), 1)
+                gameDisplay.blit(score_text,
+                                 score_text.get_rect(center=(constant.SCREEN_WIDTH / 6, constant.SCOREBOARD / 2)))
+                gameDisplay.blit(segment_text,
+                                 segment_text.get_rect(center=(constant.SCREEN_WIDTH / 1.25, constant.SCOREBOARD / 2)))
+                if alive:
+                    current = snake.head
+                else:
+                    current = snake.head.next
                 while current is not None:
                     pygame.draw.rect(gameDisplay, snake_color,
                                      (current.x, current.y, constant.SNAKE_SIZE, constant.SNAKE_SIZE), 0)
@@ -167,13 +201,19 @@ while True:
                 pygame.draw.rect(gameDisplay, background_colour,
                                  (food.x, food.y, constant.SNAKE_SIZE, constant.SNAKE_SIZE), 1)
             elif state == PAUSE:
-                gameDisplay.blit(pause_text, (100, 100))
+                gameDisplay.blit(pause_text,
+                                 pause_text.get_rect(center=(constant.SCREEN_WIDTH / 2, constant.SCREEN_HEIGHT / 2)))
 
         else:
-            gameDisplay.blit(dead_text, (100, 100))
+            if death == 0:
+                pygame.time.delay(500)
+                pygame.mixer.Sound.play(lose_sound)
+                death = 1
+            gameDisplay.blit(dead_text,
+                             dead_text.get_rect(center=(constant.SCREEN_WIDTH / 2, constant.SCREEN_HEIGHT / 2)))
 
         pygame.display.update()
-        clock.tick(constant.GAME_SPEED)
+        clock.tick(gameSpeed)
 
         # pygame.event.pump()
     restart = True
